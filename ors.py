4d937ae7dfc22873e43606373ce9e7caffa4d977
Ray <rayluo00@gmail.com>
2017-01-27 17:00:09 -0800

    Added function for historical data call

--- ors.py
+++ ors.py
@@ -1,10 +1,23 @@
 import json
 import requests
 
-def ORS_Data ():
-	ors_request = requests.get('http://api.rsbuddy.com/grandExchange?a=guidePrice&i=2434')
+def ORS_CurrentData ():
+	itemID = '2434'
+
+	ors_request = requests.get('http://api.rsbuddy.com/grandExchange?a=guidePrice&i='+itemID)
 	itemData = ors_request.json()
 	print(itemData)
 
+def ORS_HistoricalData ():
+	itemID = '2434'
+	timeInterval = '60'
+	startTime = '1474615279000'
+
+	ors_request = requests.get('https://api.rsbuddy.com/grandExchange?a=graph&g='+timeInterval+
+				'&start='+startTime+'&i='+itemID)
+	itemData = ors_request.json()
+	print(len(itemData))
+
 if __name__ == '__main__':
-	ORS_Data()
+	#ORS_CurrentData()
+	ORS_HistoricalData()
