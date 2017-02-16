c0815fd3e358fe0c574b0829d3a8c37af64b8e3d
Ray <rayluo00@gmail.com>
2017-02-16 02:56:38 -0800

    Add slope and linear regression functionality

--- ors.py
+++ ors.py
@@ -8,7 +8,11 @@ import json
 import time
 import math
 import requests
+import numpy
 import statistics
+import matplotlib
+matplotlib.use('Agg')
+import pylab
 
 ##############################################################################################
 '''
@@ -27,26 +31,48 @@ def ORS_CurrentData ():
     ors_request = requests.get('http://api.rsbuddy.com/grandExchange?a=guidePrice&i='+itemID)
     itemData = ors_request.json()
 
-def FiveNumSummary (scList, sz):
+##############################################################################################
+def FiveNumSummary (ls, sz):
 
+    # Find lower and upper quartiles
     if sz % 2 == 0:
         mid = sz / 2
-        lowq = statistics.median(scList[:math.floor(mid-1)])
-        hiq = statistics.median(scList[math.ceil(mid):])
+        lowq = statistics.median(ls[:math.floor(mid-1)])
+        hiq = statistics.median(ls[math.ceil(mid):])
     else:
         mid = int(math.ceil(sz/2))-1
-        lowq = statistics.median(scList[:mid])
-        hiq = statistics.median(scList[mid+1:])
+        lowq = statistics.median(ls[:mid])
+        hiq = statistics.median(ls[mid+1:])
     
-    med = statistics.median(scList)
+    med = statistics.median(ls)
 
-    print('min',scList[0],'\n'
+    print('min',ls[0],'\n'
             'q1',lowq,'\n'
             'median',med,'\n'
             'q3',hiq,'\n'
-            'max',scList[sz-1],'\n')
+            'max',ls[sz-1],'\n')
 
+    # Calculate range to find outliers
+    iqr = (hiq - lowq) * 1.5
+    lowestq = lowq - iqr
+    highestq = hiq + iqr
 
+##############################################################################################
+def SlopeEvaluation (ls, sz):
+
+    for i in range(1, sz):
+        slope = ls[i] - ls[i-1]
+        print(slope)
+
+def LinearRegression (ls, sz):
+    xi = numpy.arange(0,9)
+    A = numpy.array([xi, numpy.ones(9)])
+    y = [19,20,20.5,21.5,22,23,23,25.5,24]
+    w = numpy.linalg.lstsq(A.T,y)[0]
+
+    line = w[0]*xi*w[1]
+    pylab.plot(xi,line,'r-',xi,y,'o')
+    pylab.savefig('linear_reg.png')
 
 ##############################################################################################
 '''
@@ -65,23 +91,24 @@ def ORS_HistoricalData ():
     itemID = '536'
     startTime = '1357027200000'
     timeInterval = '30'
-    itemDict = {}
+    itemLs = []
+    param = 'sellingCompleted'
 
     orsRequest = requests.get('https://api.rsbuddy.com/grandExchange?a=graph&g='+timeInterval+
                                 '&start='+startTime+'&i='+itemID)
     itemData = orsRequest.json()
     sz = len(itemData)
     
-
-    itemDict['sellingCompleted'] = []
     for i in range(sz):
-        if 'sellingCompleted' in itemData[i]:
-            itemDict['sellingCompleted'].append(itemData[i]['sellingCompleted'])
+        if param in itemData[i]:
+            itemLs.append(itemData[i][param])
 
-    itemDict['sellingCompleted'].sort()
-    sclens = len(itemDict['sellingCompleted'])
+    itemLs.sort()
+    sclens = len(itemLs)
 
-    FiveNumSummary(itemDict['sellingCompleted'], sclens)
+    #FiveNumSummary(itemLs, sclens)
+    #SlopeEvaluation(itemLs, sclens)
+    LinearRegression(itemLs, sclens)
 
 if __name__ == '__main__':
     #ORS_CurrentData()
